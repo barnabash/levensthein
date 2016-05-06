bcc1036a7d7dbd6a72fb550cd0efab89cd57ad77
JU <jiri.uhyrek@zebu.cz>
2016-05-06 13:34:56 +0200

    remove def readme, added project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject barnabash/levensthein "0.9.0-SNAPSHOT"
   :description "levensthein distance between two strings"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/barnabash/levensthein"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
